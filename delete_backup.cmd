e0d4ffcf79af0dac8a2ca31cb50c800a5f7a002b
Grey-Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-14 19:54:03 +0800

    configuring the workspace

---
+++ delete_backup.cmd
@@ -0,0 +1,5 @@
+@echo off
+del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups\*.*
+cd  D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
+rd /s /q   D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
+del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\run\logs\*.gz
