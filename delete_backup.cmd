bedf00771862eebc148c5a860723b746d00b636c
Grey-Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-14 19:54:45 +0800

    Update delete_backup.cmd

--- delete_backup.cmd
+++ delete_backup.cmd
@@ -1,5 +1,5 @@
 @echo off
-del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups\*.*
-cd  D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
-rd /s /q   D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
-del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\run\logs\*.gz
+del /f /s /q D:\Create\mod_project\CGR_Fabric\.mcreator\workspaceBackups\*.*
+cd  D:\Create\mod_project\CGR_Fabric\.mcreator\workspaceBackups
+rd /s /q   D:\Create\mod_project\CGR_Fabric\.mcreator\workspaceBackups
+del /f /s /q D:\Create\mod_project\CGR_Fabric\run\logs\*.gz
